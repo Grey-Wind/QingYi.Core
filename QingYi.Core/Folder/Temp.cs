e4614c52319e026ef39820f52730d876c60f16db
Grey Wind <wjunqin7@gmail.com>
2024-09-09 08:53:57 +0800

    4.0.0 (#1)
    * optimize namespace
    
    * 合并文件
    
    * 恢复图标

---
+++ QingYi.Core/Folder/Temp.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace Folder
+{
+    public class Temp
+    {
+        /// <summary>
+        /// Get the Temp folder<br></br>
+        /// 获取Temp文件夹
+        /// </summary>
+        /// <returns>
+        /// Temp folder<br></br>
+        /// Temp文件夹路径
+        /// </returns>
+        public static string Get()
+        {
+            return Path.GetTempPath();
+        }
+
+        public static string CreateFile(string fileName)
+        {
+            string filePath = Path.Combine(Get(), fileName);
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+
+            return filePath;
+        }
+
+        /// <summary>
+        /// Create a new subfolder within the Temp folder<br></br>
+        /// 在Temp文件夹内创建新的子文件夹
+        /// </summary>
+        /// <param name="newFolderName">
+        /// The name of the new folder you want to create<br></br>
+        /// 想要创建的新文件夹名称
+        /// </param>
+        public static string CreateFolder(string newFolderName)
+        {
+            string name = Path.Combine(Get(), newFolderName);
+            Directory.CreateDirectory(name);
+
+            return name;
+        }
+    }
+}
