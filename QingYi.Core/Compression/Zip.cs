d0b5b3567dddc14349ef10268b359ad601ec8097
Grey Wind <wjunqin7@gmail.com>
2025-02-10 20:39:17 +0800

    添加压缩支持

---
+++ QingYi.Core/Compression/Zip.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace QingYi.Core.Compression
+{
+    public struct Zip
+    {
+        public static void ArchiveFile(string sourceFile)
+        {
+            string zipFile = Path.Combine(Path.GetDirectoryName(sourceFile), Path.GetFileName(sourceFile) + ".zip");
+
+            using FileStream fs = new FileStream(zipFile, FileMode.Create);
+            using ZipArchive archive = new ZipArchive(fs, ZipArchiveMode.Create);
+            archive.CreateEntryFromFile(sourceFile, Path.GetFileName(sourceFile));
+        }
+
+        public static void ArchiveFile(string sourceFile, string zipFile)
+        {
+            using FileStream fs = new FileStream(zipFile, FileMode.Create);
+            using ZipArchive archive = new ZipArchive(fs, ZipArchiveMode.Create);
+            archive.CreateEntryFromFile(sourceFile, Path.GetFileName(sourceFile));
+        }
+
+        public static void ArchiveFolder(string sourceFolder)
+        {
+            string zipFile = Path.Combine(Path.GetDirectoryName(sourceFolder), Path.GetFileName(sourceFolder) + ".zip");
+
+            ZipFile.CreateFromDirectory(sourceFolder, zipFile, CompressionLevel.Optimal, true);
+        }
+
+        public static void ArchiveFolder(string sourceFolder, CompressionLevel compressionLevel)
+        {
+            string zipFile = Path.Combine(Path.GetDirectoryName(sourceFolder), Path.GetFileName(sourceFolder) + ".zip");
+
+            ZipFile.CreateFromDirectory(sourceFolder, zipFile, compressionLevel, true);
+        }
+
+        public static void ArchiveFolder(string sourceFolder, string zipFile)
+        {
+            ZipFile.CreateFromDirectory(sourceFolder, zipFile, CompressionLevel.Optimal, true);
+        }
+
+        public static void ArchiveFolder(string sourceFolder, string zipFile, CompressionLevel compressionLevel)
+        {
+            ZipFile.CreateFromDirectory(sourceFolder, zipFile, compressionLevel, true);
+        }
+    }
+}
