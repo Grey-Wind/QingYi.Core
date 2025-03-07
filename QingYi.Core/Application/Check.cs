7024083e72d8c022434b95a6f37ec33c786f8ebd
Grey Wind <wjunqin7@gmail.com>
2025-03-07 16:28:52 +0800

    Added the ability to detect if an application is running.

---
+++ QingYi.Core/Application/Check.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace QingYi.Core.Application
+{
+    /// <summary>
+    /// Provides methods to check the status of running applications on the system.
+    /// </summary>
+    public static class Check
+    {
+        /// <summary>
+        /// Checks if the specified application is running on the system.
+        /// </summary>
+        /// <param name="app">The name of the application to check (without the file extension).</param>
+        /// <returns>Returns true if the application is running, otherwise false.</returns>
+        public static bool CheckRunning(string app)
+        {
+            // Get all running processes and check if the specified process is found
+            var processes = Process.GetProcessesByName(app);
+
+            // Use relational pattern in C# 9.0 or higher
+            if (processes.Length > 0)
+            {
+                Console.WriteLine($"{app}.exe is running.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{app}.exe is not running.");
+                return false;
+            }
+        }
+    }
+}
